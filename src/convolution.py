7dc369bc0261a2e13cc7af95172f811c2a9fd566
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-08-20 01:05:19 +0200

    more data

--- src/convolution.py
+++ src/convolution.py
@@ -3,18 +3,18 @@ import numpy as np
 from numpy.typing import NDArray
 from scipy.integrate import quad
 
-def step_function(x: float, norm: float = 1., lower: float = 0, upper: float = 2) -> float:
+def step_function(x: float, norm: float = 1., lower: float = 0, upper: float = 2, zero: float = 0.0) -> float:
     if x < lower:
-        return 0
+        return zero
     if x > upper:
-        return 0
+        return zero
     return norm / (upper - lower)
 
-def step_functions(x: NDArray, norm: float = 1., lower: float = 0, upper: float = 2) -> NDArray:
+def step_functions(x: NDArray, norm: float = 1., lower: float = 0, upper: float = 2, zero: float = 0.0) -> NDArray:
     '''Array version of step_function'''
     result = norm / (upper - lower) * np.ones_like(x)
-    result[x < lower] = 0
-    result[x > upper] = 0
+    result[x < lower] = zero
+    result[x > upper] = zero
     return result
 
 def function_norm(func: Callable, lower_limit : float = -np.inf, upper_limit : float = np.inf, args: tuple = ()) -> float:
@@ -27,9 +27,9 @@ def convolution_integrand(func: Callable) -> Callable:
     return func(t, *args, **kwargs) * func(y-t, *args, **kwargs)
   return _convolution_integrand
 
-def convolution(func: Callable, lower_limit : float = -np.inf, upper_limit : float = np.inf) -> Callable:
+def convolution(func: Callable, lower_limit : float = -np.inf, upper_limit : float = np.inf, args: tuple = (), kwargs: dict = {}) -> Callable:
   '''Return the convolution of func with itself.'''  
-  def _convolution(y: float, args: tuple = (), kwargs: dict = {}) -> float:
+  def _convolution(y: float, integrand_args: tuple = ()) -> float:
     integrand = convolution_integrand(func)
-    return quad(func=integrand, a=lower_limit, b=upper_limit, args=(y, *args), limit=500, epsabs=1e-5, epsrel=1e-4)[0]
+    return quad(func=integrand, a=lower_limit, b=upper_limit, args=(y, integrand_args), *args, **kwargs)
   return _convolution
